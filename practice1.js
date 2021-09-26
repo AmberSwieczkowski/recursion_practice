4ccc0b100e8e35d2e21dcfa36ec8fdfac0c02721
Amber M <amberm31@protonmail.com>
2021-09-26 12:15:39 -0500

    updated practice1

--- practice1.js
+++ practice1.js
@@ -4,7 +4,6 @@ is the product of all positive integers less than or equal to n.
 For example, 5! = 5 x 4 x 3 x 2 x 1 = 120 */
 
 function factorial(n) {
-    let answer = 1
     if(n == 0 || n == 1) {
         return n;
     }
